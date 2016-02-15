a3b884ebb399d3512e97b16b1e91d38f60eb2c96
Jusqu123 <jusquu123@gmail.com>
2016-02-15 22:09:34 +0200

    HEATER

---
+++ heaterrr/heaterrr/Heater.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace heaterrr
+{
+  
+    class Heater
+    {
+        private double temperature { get; set; }
+
+        private double humidity { get; set; }
+
+
+        public double Temperature
+        {
+
+
+            get
+            {
+                return temperature;
+            }
+
+
+            set
+            {
+                if (value <= 120)
+                    temperature = value;
+            }
+        }
+
+        public double Humidity
+        {
+
+
+            get
+            {
+                return humidity;
+            }
+
+
+            set
+            {
+                if (value <= 100)
+                    humidity = value;
+            }
+        }
+
+
+        public double Value { get; set; }
+
+
+
+        public string SetTemperature(double value)
+        {
+            Temperature = value;
+           
+ return Temperature.ToString();
+        }
+
+
+
+        public string SetHumidity(double value)
+        {
+            Humidity = value;
+            return Humidity.ToString();
+        }
+    }
+}
